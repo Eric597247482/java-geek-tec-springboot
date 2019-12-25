402b873ac32e5c9819db2b64df6d7dc77e819fc7
Eric <hakmwtx@126.com>
2019-12-25 23:09:04 +0800

    静态资源配置+拦截器+MessageConverters

--- javassm/src/main/java/org/javaboy/controller/HelloController.java
+++ javassm/src/main/java/org/javaboy/controller/HelloController.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @Author Eric
  * @Date: 2019/12/22
@@ -16,5 +19,14 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return HelloService.sayHello();
+    }
+
+    @GetMapping("/data")
+    public List<String> getData() {
+        ArrayList<String> arrayList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            arrayList.add("hello java --" + i );
+        }
+        return arrayList;
     }
 }
